025a59182fca2e90a43fbf4618bc746530554284
Héctor Zaragoza <hector.zaragoza.arranz@gmail.com>
2016-05-03 08:56:39 +0200

    hector mola maaaaas

--- Barctor_GIT/src/HolaHector.java
+++ Barctor_GIT/src/HolaHector.java
@@ -2,7 +2,7 @@ public class HolaHector {
 	public static void main(String[] args) {
 		while(true){
 			System.out.println("BAR MOLA");
-			System.out.println("TOR MOLA");
+			System.out.println("TOR MOLA AÚN MÁS, ZORRA");
 		}
 	
 	}
